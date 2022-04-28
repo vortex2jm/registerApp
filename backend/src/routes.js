8a1d550ec6ce6aabcf3278d9c57ef7039d2edb10
vortex2jm <joaopaulomoura5328@gmail.com>
2022-04-28 11:21:36 -0300

    Adding delete route and login route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,14 +3,19 @@ const routes = express.Router();
 
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionControler = require('./controllers/sessionController');
 
-// List route
+
 routes.get('/ongs', ongController.index);
-// Create route
 routes.post('/ongs', ongController.create);
 
 routes.post('/incidents', incidentController.create);
-
 routes.get('/incidents', incidentController.index);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.index);
+
+routes.post('/sessions', sessionControler.create);
 
 module.exports = routes;
